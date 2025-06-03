21cf14b9788137a33fce52f8f2f48e43351f59c1
VincentComier <V.Comier@hotmail.fr>
2025-06-03 10:15:35 +0200

    correction exercice debug

--- syntaxe/debug.js
+++ syntaxe/debug.js
@@ -1,3 +1,4 @@
+/*
 // Nous souhaitons faire la somme du prix de plusieurs articles
 // Prix de deux articles
 let a1 = 10
@@ -13,4 +14,14 @@ console.log("Le prix total avant reduction est de : ", tot)
 console.log("Le prix total après reduction est de : ", tot2)
 
 //Nous appliquons 20% de reduction
+let tot2 = tot - tot*0.2 */
+
+let a1 = 10
+let a2 = 20
+// Nous calculons le total
+let tota = a1 + a2
+let a3 = 50
+tot = tota + a3
 let tot2 = tot - tot*0.2
+console.log("le prix total avant reduction est de : ",tot)
+console.log("le prix total apres reduction est de : ",tot2)
