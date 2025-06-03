3f42e86a540f308ef0f8f3aff334e3c50f4695fb
VincentComier <V.Comier@hotmail.fr>
2025-06-03 15:52:58 +0200

    exo declaration termine

---
+++ exoVariables/script.js
@@ -0,0 +1,8 @@
+let moi = {
+    nom : "comier",
+    prenom : "vincent",
+}
+const favorite = 64
+let age = 31
+alert(moi)
+alert(favorite - age)
---
+++ syntaxe/declaration.js
@@ -0,0 +1,7 @@
+let nom = "comier"
+let prenom = "vincent"
+alert("bonjour" + " " + nom + " " + prenom)
+const favorite = 64
+let age = 31
+let result = favorite - age
+alert(result + " " + "années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ")
